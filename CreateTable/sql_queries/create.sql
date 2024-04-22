e62753f3d05e00e1a57d86a4dff6b6502bad1f15
sgorgun <sergei.gorgun@gmail.com>
2024-04-22 18:56:53 +0300

    Done

--- CreateTable/sql_queries/create.sql
+++ CreateTable/sql_queries/create.sql
@@ -5,15 +5,9 @@ CREATE TABLE Manufacturers (
 
 CREATE TABLE Brands (
     BrandID INT PRIMARY KEY,
-    BrandName NVARCHAR(100) NOT NULL UNIQUE
-);
-
-CREATE TABLE ManufacturerBrands (
+    BrandName NVARCHAR(100) NOT NULL UNIQUE,
     ManufacturerID INT NOT NULL,
-    BrandID INT NOT NULL,
-    PRIMARY KEY (ManufacturerID, BrandID),
-    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID),
-    FOREIGN KEY (BrandID) REFERENCES Brands(BrandID)
+    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
 );
 
 CREATE TABLE ScreenResolutions (
@@ -29,7 +23,9 @@ CREATE TABLE MatrixTypes (
 
 CREATE TABLE RAMOptions (
     RAMID INT PRIMARY KEY,
-    RAMSize INT NOT NULL
+    RAMSize INT NOT NULL,
+    ManufacturerID INT NOT NULL,
+    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
 );
 
 CREATE TABLE ROMOptions (
@@ -39,21 +35,27 @@ CREATE TABLE ROMOptions (
 
 CREATE TABLE ModelNames (
     ModelNameID INT PRIMARY KEY,
-    ModelName NVARCHAR(100) NOT NULL UNIQUE
+    ModelName NVARCHAR(100) NOT NULL UNIQUE,
+    BrandID INT NOT NULL,
+    FOREIGN KEY (BrandID) REFERENCES Brands(BrandID)
+);
+
+CREATE TABLE Screen (
+    ScreenID INT PRIMARY KEY,
+    ScreenResolutionID INT NOT NULL,
+    MatrixTypeID INT NOT NULL,
+    FOREIGN KEY (ScreenResolutionID) REFERENCES ScreenResolutions(ScreenResolutionID),
+    FOREIGN KEY (MatrixTypeID) REFERENCES MatrixTypes(MatrixTypeID)
 );
 
 CREATE TABLE Models (
     ModelID INT PRIMARY KEY,
     ModelNameID INT NOT NULL,
-    BrandID INT NOT NULL,
-    ScreenResolutionID INT,
-    MatrixTypeID INT,
+    ScreenID INT,
     RAMID INT,
     ROMID INT,
     FOREIGN KEY (ModelNameID) REFERENCES ModelNames(ModelNameID),
-    FOREIGN KEY (BrandID) REFERENCES Brands(BrandID),
-    FOREIGN KEY (ScreenResolutionID) REFERENCES ScreenResolutions(ScreenResolutionID),
-    FOREIGN KEY (MatrixTypeID) REFERENCES MatrixTypes(MatrixTypeID),
+    FOREIGN KEY (ScreenID) REFERENCES Screen(ScreenID),
     FOREIGN KEY (RAMID) REFERENCES RAMOptions(RAMID),
     FOREIGN KEY (ROMID) REFERENCES ROMOptions(ROMID)
 );
@@ -63,24 +65,30 @@ CREATE TABLE Positions (
     PositionName NVARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY,
-    PositionID INT NOT NULL,
+CREATE TABLE Person (
+    PersonID INT PRIMARY KEY,
     LastName NVARCHAR(100) NOT NULL,
     FirstName NVARCHAR(100) NOT NULL,
-    FOREIGN KEY (PositionID) REFERENCES Positions(PositionID)
+    PhoneNumber NVARCHAR(15) UNIQUE
 );
 
-CREATE TABLE Customers (
-    CustomerID INT PRIMARY KEY,
-    LastName NVARCHAR(100) NOT NULL,
-    FirstName NVARCHAR(100) NOT NULL,
-    PhoneNumber NVARCHAR(15) NOT NULL UNIQUE
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    PersonID INT NOT NULL,
+    PositionID INT NOT NULL,
+    FOREIGN KEY (PersonID) REFERENCES Person(PersonID),
+    FOREIGN KEY (PositionID) REFERENCES Positions(PositionID)
 );
 
 CREATE TABLE OrderStates (
     StateID INT PRIMARY KEY,
     StateName NVARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    PersonID INT NOT NULL,
+    FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
 );
 
 CREATE TABLE Smartphones (
@@ -89,22 +97,27 @@ CREATE TABLE Smartphones (
     ManufactureYear INT NOT NULL CHECK (ManufactureYear >= 1980),
     ImeiA NVARCHAR(15) UNIQUE NOT NULL,
     ImeiB NVARCHAR(15) UNIQUE,
-    OwnerID INT NOT NULL,
+    OwnerID INT UNIQUE,
     FOREIGN KEY (ModelID) REFERENCES Models(ModelID),
     FOREIGN KEY (OwnerID) REFERENCES Customers(CustomerID)
 );
 
-CREATE TABLE Repairs (
-    RepairID INT PRIMARY KEY,
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
     SmartphoneID INT NOT NULL,
-    ReceiptNumber NVARCHAR(50) NOT NULL UNIQUE,
     DiagnosingEmployeeID INT NOT NULL,
-    RepairEmployeeID INT,
-    MalfunctionDescription NVARCHAR(255) NOT NULL,
     OrderStateID INT NOT NULL,
-    RepairCost DECIMAL(10, 2) CHECK (RepairCost >= 0),
     FOREIGN KEY (SmartphoneID) REFERENCES Smartphones(SmartphoneID),
     FOREIGN KEY (DiagnosingEmployeeID) REFERENCES Employees(EmployeeID),
-    FOREIGN KEY (RepairEmployeeID) REFERENCES Employees(EmployeeID),
     FOREIGN KEY (OrderStateID) REFERENCES OrderStates(StateID)
+);
+
+CREATE TABLE Repairs (
+    RepairID INT PRIMARY KEY,
+    OrderID INT NOT NULL,
+    RepairEmployeeID INT,
+    MalfunctionDescription NVARCHAR(255) NOT NULL,
+    RepairCost DECIMAL(10, 2) CHECK (RepairCost >= 0),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (RepairEmployeeID) REFERENCES Employees(EmployeeID)
 );
